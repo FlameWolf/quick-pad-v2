b58943b10d4eb0d485f09f0a062994bdfcb87a9c
FlameWolf <agnijith@gmail.com>
2026-07-07 09:43:38 +0530

    Exclude alert variant classes from being purged

--- src/components/NotificationList.tsx
+++ src/components/NotificationList.tsx
@@ -1,16 +1,19 @@
+import { For } from "solid-js";
 import { notifications, removeNotification } from "@/stores/notifications";
 
 export default function NotificationList() {
 	return (
 		<div class="d-flex flex-column gap-1 notification-list position-fixed end-0 bottom-0 me-1 mb-1">
-			{notifications().map(notification => (
-				<div class={`alert fade show alert-${notification.type}`} role="alert">
-					<div class="d-flex">
-						<div class="me-auto" innerHTML={notification.message}></div>
-						<button class="btn-close ms-2" onClick={() => removeNotification(notification.id)} aria-label="Close"></button>
+			<For each={notifications()}>
+				{notification => (
+					<div class="alert fade show" classList={{ [`alert-${notification.type}`]: true }} role="alert">
+						<div class="d-flex">
+							<div class="me-auto" innerHTML={notification.message}></div>
+							<button class="btn-close ms-2" onClick={() => removeNotification(notification.id)} aria-label="Close"></button>
+						</div>
 					</div>
-				</div>
-			))}
+				)}
+			</For>
 		</div>
 	);
 }

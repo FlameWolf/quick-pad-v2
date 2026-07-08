e16b0679f41c9fb7e8c5ecb87bc7c4e515b27f86
FlameWolf <agnijith@gmail.com>
2026-07-08 10:02:28 +0530

    Ensure notifications always take up minimum width and stay anchored to right

--- src/components/NotificationList.tsx
+++ src/components/NotificationList.tsx
@@ -8,9 +8,9 @@ export default function NotificationList() {
 		<div class="d-flex flex-column gap-2 notification-list position-fixed end-0 bottom-0 me-2 mb-2">
 			<For each={sortedNotifications()}>
 				{notification => (
-					<div class="alert m-0" classList={{ [`alert-${notification.type}`]: true }} role="alert">
+					<div class="alert m-0 ms-auto" classList={{ [`alert-${notification.type}`]: true }} role="alert">
 						<div class="d-flex">
-							<div class="me-auto" innerHTML={notification.message}></div>
+							<div innerHTML={notification.message}></div>
 							<button class="btn-close ms-2" onClick={() => removeNotification(notification.id)} aria-label="Close"></button>
 						</div>
 					</div>

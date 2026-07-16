425485297696660e3b600a90784b6c898006a63d
FlameWolf <agnijith@gmail.com>
2026-07-16 15:03:36 +0530

    Refactor stores

--- src/stores/notifications.ts
+++ src/stores/notifications.ts
@@ -1,16 +1,15 @@
 import { createStore, produce } from "solid-js/store";
 import type { UUID } from "crypto";
 
-export type Notification = {
+type Notification = {
 	id: UUID;
 	type: "success" | "info" | "warning" | "danger";
 	timeStamp: number;
 	message: string;
 	removeTimer?: ReturnType<typeof setTimeout>;
 };
-export type NotificationList = Array<Notification>;
+type NotificationList = Array<Notification>;
 
-const maxNotifications = 5;
 const [store, setStore] = createStore<NotificationList>([]);
 export const notifications = () => store;
 
@@ -23,25 +22,22 @@ function createNotification(type: Notification["type"], message: string) {
 	};
 	if (type !== "danger") {
 		notification.removeTimer = setTimeout(() => {
-			deleteNotification(notification.id);
+			deleteNotification(notification);
 		}, 5000);
 	}
-	if (store.length >= maxNotifications) {
-		deleteNotification(store[0]!.id);
+	if (store.length >= 5) {
+		deleteNotification(store[0]!);
 	}
 	setStore(store.concat(notification));
 }
 
-function deleteNotification(id: UUID) {
-	const index = store.findIndex(n => n.id === id);
-	if (index !== -1) {
-		clearTimeout(store[index]!.removeTimer);
-		setStore(
-			produce(store => {
-				store.splice(index, 1);
-			})
-		);
-	}
+function deleteNotification(notification: Notification) {
+	clearTimeout(notification.removeTimer);
+	setStore(
+		produce(store => {
+			store.splice(store.indexOf(notification), 1);
+		})
+	);
 }
 
 export function addNotification(type: Notification["type"], message: string) {
@@ -50,10 +46,13 @@ export function addNotification(type: Notification["type"], message: string) {
 		createNotification(type, message);
 		return;
 	}
-	deleteNotification(existingNotification.id);
+	deleteNotification(existingNotification);
 	setTimeout(() => createNotification(type, message), 250);
 }
 
 export function removeNotification(id: UUID) {
-	deleteNotification(id);
+	const notification = store.find(n => n.id === id);
+	if (notification) {
+		deleteNotification(notification);
+	}
 }

08c6eb96aeade69d47185866b4ac5f24b66f4103
FlameWolf <agnijith@gmail.com>
2026-07-28 12:35:22 +0530

    Fix tags not being updated when exiting edit mode

--- src/components/DisplayTagList.tsx
+++ src/components/DisplayTagList.tsx
@@ -139,15 +139,26 @@ export default function DisplayTagList(props: Props) {
 		on(
 			isSelecting,
 			(curr, prev) => {
-				if (prev === false) {
+				if (!prev) {
 					lastSelected = Array.from(selectedTags());
 				}
-				if (curr === false) {
+				if (!curr) {
 					setSelectedTags(Array.from(lastSelected));
 				}
 				props.onSelectionChanged?.(selectedTags());
 			},
 			{ defer: true }
+		)
+	);
+
+	createEffect(
+		on(
+			() => props.allowEdit,
+			value => {
+				if (!value) {
+					setSelectedTags(props.activeTags ?? []);
+				}
+			}
 		)
 	);
 

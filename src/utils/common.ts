7bd106bed80e6d75ffc4b66838d51d671828cb49
FlameWolf <agnijith@gmail.com>
2026-07-28 15:07:20 +0530

    Auto-populate new note tags with current filter

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -13,12 +13,16 @@ export function normaliseTag(raw: string): string {
 	return raw.trim().replace(/\s+/g, " ").normalize("NFC");
 }
 
-export function haveSameItems<T>(a: readonly T[] = [], b: readonly T[] | undefined = []): boolean {
+export function areArraysEqual<T>(a: readonly T[] = [], b: readonly T[] | undefined = []): boolean {
 	if (a.length !== b.length) {
 		return false;
 	}
 	const set = new Set(a);
 	return b.every(x => set.has(x));
+}
+
+export function areSetsEqual(setA: Set<unknown>, setB: Set<unknown>) {
+	return setA.symmetricDifference(setB).size === 0;
 }
 
 export function copyNullableArray<T>(arr: T[] | undefined): T[] | undefined {

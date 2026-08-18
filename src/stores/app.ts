d36d9d239aa2c61bb0374b0234a6b934fc7e7717
FlameWolf <agnijith@gmail.com>
2026-08-18 16:49:38 +0530

    Update app store to include `currentColour` property and its getter/setter utilities

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -5,14 +5,17 @@ import { FONT_SCALE_FACTOR } from "@/constants/ui";
 
 interface AppState {
 	lastView: View | null | undefined;
+	currentColour: Colour | undefined;
 	fontScaleFactor: number;
 }
 
 const [store, setStore] = createStore<AppState>({
 	lastView: undefined,
+	currentColour: undefined,
 	fontScaleFactor: getFontScaleFactor()
 });
 export const lastView = createMemo(() => store.lastView);
+export const currentColour = createMemo(() => store.currentColour);
 export const fontScaleFactor = createMemo(() => store.fontScaleFactor);
 
 function getFontScaleFactor(): number {
@@ -25,6 +28,10 @@ function getFontScaleFactor(): number {
 
 export function setLastView(view: View | null | undefined) {
 	setStore("lastView", view);
+}
+
+export function setCurrentColour(colour: Colour | undefined) {
+	setStore("currentColour", colour);
 }
 
 export function setFontScaleFactor(factor: number) {

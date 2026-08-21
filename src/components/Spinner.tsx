f197d2b208f2dd59bb7c0568ec55685203e1b7dc
FlameWolf <agnijith@gmail.com>
2026-08-21 12:36:32 +0530

    Refactor code

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,4 +1,4 @@
-import { Show, mergeProps } from "solid-js";
+import { Show, createMemo } from "solid-js";
 import { Dynamic } from "solid-js/web";
 
 type Props = {
@@ -8,14 +8,14 @@ type Props = {
 	tag?: string;
 };
 
-export default function Spinner(rawProps: Props) {
-	const props = mergeProps({ showMessage: true }, rawProps);
+export default function Spinner(props: Props) {
+	const showMessage = createMemo(() => props.showMessage ?? true);
 
 	return (
 		<Show when={!props.minimal} fallback={<Dynamic component={props.tag ?? "div"} class="spinner-border spinner-border-sm" role="status"/>}>
-			<div class="d-flex flex-column justify-content-center align-items-center" classList={{ "py-3": !props.showMessage }}>
-				<div class="spinner-border" aria-hidden="true" aria-label={props.showMessage ? undefined : props.message}/>
-				<Show when={props.showMessage}>
+			<div class="d-flex flex-column justify-content-center align-items-center" classList={{ "py-3": !showMessage() }}>
+				<div class="spinner-border" aria-hidden="true" aria-label={showMessage() ? undefined : props.message}/>
+				<Show when={showMessage()}>
 					<div class="mt-3" role="status">{props.message ?? "Loading..."}</div>
 				</Show>
 			</div>
